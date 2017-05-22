a340a4d7cb1108c47930d9056156981d36c36d52
AJ Haggard <ahaggard2013@my.fit.edu>
2017-05-22 10:06:19 -0400

    fixed shadowing

--- randomUserNames.py
+++ randomUserNames.py
@@ -2,12 +2,12 @@
 import random
 import string
 
-list = open('redditNameList.txt', 'w')
 
-def generate_username_password(amount, ulength, plength):
+def generate_username_password(amount, ulength, plength, list):
     for _ in range(0,amount):
         list.write(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(ulength)) + ':'+ ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(plength)) + '\n')
 
         
-generate_username_password(100,15,15)
+list = open('redditNameList.txt', 'w')
+generate_username_password(100,15,15, list)
 list.close()
