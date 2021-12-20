b41f1387ea6147f2fbe576791051af64c3f183d7
codewithnick <nikhilsingh526452@gmail.com>
2021-12-20 10:59:47 +0530

    fixed creator.py

--- creator.py
+++ creator.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from random import randint
 import time 
 import os
+from selenium.common.exceptions import NoSuchElementException
 
 
 
@@ -13,31 +14,33 @@ createdUserNamePasswordFile = 'createdNames.txt'
 def create_account(username, password):
     #set up profile for proxy
     profile = webdriver.FirefoxProfile()
-    profile.set_preference("network.proxy.type", 1)
+    '''profile.set_preference("network.proxy.type", 1)
     profile.set_preference("network.proxy.socks", '127.0.0.1')
     profile.set_preference("network.proxy.socks_port", 9050)
-    profile.set_preference("network.proxy.socks_remote_dns", True)
+    profile.set_preference("network.proxy.socks_remote_dns", True)'''
     profile.update_preferences()
     browser = webdriver.Firefox(firefox_profile=profile)
 
     #get reddit account creation page
-    browser.set_window_size(683, 744)
-    browser.get('http://reddit.com/login')
+    browser.get('https://www.reddit.com/register/')
+    print("waiting for you to enter a email")
+    time.sleep(randint(2,3))
+    browser.find_element_by_id('regEmail').send_keys(username+"@cds.com"+'\n')
+    browser.find_element_by_xpath("//button[contains(text(), 'Continue')]").click()
     #insert username
     time.sleep(randint(1,5))
-    browser.find_element_by_id('user_reg').click()
-    browser.find_element_by_id('user_reg').send_keys(username)
+    browser.find_element_by_id('regUsername').click()
+    browser.find_element_by_id('regUsername').send_keys(username)
     #insert password
     time.sleep(randint(1,5))
-    browser.find_element_by_id('passwd_reg').click()
-    browser.find_element_by_id('passwd_reg').send_keys(password)
+    browser.find_element_by_id('regPassword').click()
+    browser.find_element_by_id('regPassword').send_keys(password)
     time.sleep(randint(1,5))
-    browser.find_element_by_id('passwd2_reg').click()
-    browser.find_element_by_id('passwd2_reg').send_keys(password)
+    browser.find_element_by_xpath("//*[contains(text(), 'Sign Up')]").click()
     #pause to manually enter captcha
-    input = raw_input("[*] Solve captcha, create account, then press enter... enter 'r' as input if captcha doesn't appear to skip username" + '\n')
-    if (input == 'r'):
-        os.system('clear')
+    myinput = input("[*] Solve captcha, create account, then press enter... enter 'r' as input if captcha doesn't appear to skip username" + '\n')
+    if (myinput == 'r'):
+        
         browser.quit()
         return False
     else:
@@ -53,6 +56,7 @@ def main():
     #run account generator for each user in list
     created = open(createdUserNamePasswordFile, 'a')
     creds = [cred.strip() for cred in open(userNamePasswordFile).readlines()]
+    global username,password
     for cred in creds:
         username, password = cred.split(':')
         print('[+] creating account for %s with password %s' % (username,password))
@@ -72,7 +76,7 @@ def main():
         else:
             print('[-] name not recorded due to captcha issue')
         time.sleep(2)
-        os.system('clear')
+        
     created.close()
 
     
